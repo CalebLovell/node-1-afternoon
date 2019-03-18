92650d379236ac5b525c619b931ea255f22011d4
CalebLovell <39709791+CalebLovell@users.noreply.github.com>
2019-03-18 15:02:56 -0600

    step 5 finish

--- server/controllers/messages_controller.js
+++ server/controllers/messages_controller.js
@@ -4,15 +4,30 @@ let id = 0;
 
 module.exports = {
     create: (req, res) => {
-        res.status(200).send(messages)
+        const { text, time } = req.body;
+        messages.push({ id, text, time });
+        id++;
+        res.status(200).send(messages);
     },
     read: (req, res) => {
-        res.status(200).send(messages)
+        res.status(200).send(messages);
     },
     update: (req, res) => {
-        res.status(200).send(messages)
+        const { text } = req.body;
+        const updateID = req.params.id;
+        const correctMessage = messages.findIndex( message => message.id == updateID );
+        let message = messages[correctMessage];
+        messages[correctMessage] = {
+            id: message.id,
+            text: text || message.text,
+            time: message.time,
+        };
+        res.status(200).send(messages);
     },
     delete: (req, res) => {
-        res.status(200).send(messages)
+        const deleteID = req.params.id;
+        correctMessage = messages.findIndex( message => message.id == deleteID );
+        messages.splice(correctMessage, 1);
+        res.status(200).send(messages);
     },
-}
+};
--- server/index.js
+++ server/index.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mc = require('./controllers/messages_controller');
 
 const app = express();
 
 app.use(bodyParser.json());
+
+const baseUrl = '/api/messages'
+app.post(`${baseUrl}`, mc.create);
+app.get(`${baseUrl}`, mc.read);
+app.put(`${baseUrl}/:id`, mc.update);
+app.delete(`${baseUrl}/:id`, mc.delete);
 
 const PORT = 7999;
 app.listen(PORT, () => console.log(`Server is listening on ${PORT}`));
